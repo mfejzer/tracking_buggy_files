5175ef6fd602ecea9905f038dd5abd21aa0f20e9
Mikołaj Fejzer <mfejzer@mat.umk.pl>
2019-07-08 19:39:02 +0200

    Fix process_bug_reports.py

--- process_bug_reports.py
+++ process_bug_reports.py
@@ -4,15 +4,13 @@
 """Usage: %(scriptName) <bug_reports.xml> <repository_path> <bug_reports.json>
 """
 
-import collections 
-import itertools
-import psutil
 import subprocess
 import sys
 import xml.etree.ElementTree as ET
 
-from json import dumps
+from json import dump
 from tqdm import tqdm
+
 
 def main():
     bug_reports_file = sys.argv[1]
@@ -20,46 +18,38 @@ def main():
     json_file_name = sys.argv[3]
     dataset = load_dataset(bug_reports_file, repository)
     with open(json_file_name, 'w') as f:
-        json.dump(dataset, f)
-    # print(dumps(dataset))
+        dump(dataset, f)
 
 
 def load_dataset(bug_reports_file, repository):
-    dataset = {} 
+    dataset = {}
     tree = ET.parse(bug_reports_file)
     root = tree.getroot()
     for database in root.findall('database'):
         for bug_report in tqdm(database.findall('table')):
             fixing_commit = bug_report.find("column[@name='commit']").text
-            bug_report_content = {}
-            bug_report_content['commit'] = fixing_commit
-            bug_report_content['id'] = bug_report.find("column[@name='id']").text
-            bug_report_content['bug_id'] = bug_report.find("column[@name='bug_id']").text
-            bug_report_content['summary'] = bug_report.find("column[@name='summary']").text
-            bug_report_content['timestamp'] = bug_report.find("column[@name='report_timestamp']").text
-            bug_report_content['status'] = bug_report.find("column[@name='status']").text
-            bug_report_content['result'] = bug_report.find("column[@name='result']").text
-            bug_report_content['description'] = bug_report.find("column[@name='description']").text
-             
+            bug_report_content = {'commit': fixing_commit,
+                                  'id': bug_report.find("column[@name='id']").text,
+                                  'bug_id': bug_report.find("column[@name='bug_id']").text,
+                                  'summary': bug_report.find("column[@name='summary']").text,
+                                  'timestamp': bug_report.find("column[@name='report_timestamp']").text,
+                                  'status': bug_report.find("column[@name='status']").text,
+                                  'result': bug_report.find("column[@name='result']").text,
+                                  'description': bug_report.find("column[@name='description']").text}
+
             commit_content = retrieve_commit(repository, fixing_commit)
-           
-            dataset[fixing_commit] = {'bug_report':bug_report_content, 'commit':commit_content}
+
+            dataset[fixing_commit] = {'bug_report': bug_report_content, 'commit': commit_content}
 
     return dataset
 
-#    with open(fixes_file) as fixes_file_handle:
-#        for line in tqdm(fixes_file_handle):
-#            fix = line.decode('latin-1').rstrip()
-#            dataset[fix] = retrieve_commit(repository, fix)
-#    return dataset
 
 def retrieve_commit(repository, commit, ext='.java'):
     metadata = retrieve_metadata(repository, commit)
     diff = retrieve_diff(repository, commit, ext=ext)
-    commit_content = {}
-    commit_content['metadata'] = metadata
-    commit_content['diff'] = diff
+    commit_content = {'metadata': metadata, 'diff': diff}
     return commit_content
+
 
 def retrieve_metadata(repository, commit):
     full_sha = None
@@ -67,46 +57,37 @@ def retrieve_metadata(repository, commit):
     date = None
     message = ''
     cmd = 'git -C ' + repository + ' show -s ' + commit
-#    print cmd
-    process = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
-    for index, line in enumerate(iter(process.stdout.readline, '')):
-        line = line.decode('latin-1')
+    show_lines = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('latin-1').split('\n')
+    for index, line in enumerate(show_lines):
         if index == 0:
-             full_sha = line
+            full_sha = line
         elif index == 1:
-             author = line
+            author = line
         elif index == 2:
-             date = line
+            date = line
         else:
-             message += line
-    metadata = {}
-    metadata['sha'] = full_sha
-    metadata['author'] = author
-    metadata['date'] = date
-    metadata['message'] = message
+            message += line
+    metadata = {'sha': full_sha, 'author': author, 'date': date, 'message': message}
     return metadata
-        
+
 
 def retrieve_diff(repository, commit, ext='.java'):
     cmd = 'git -C ' + repository + ' diff-tree --no-commit-id --name-only -r ' + commit
-#    print cmd
+    #    print cmd
     files = {}
-    process = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
-    for line in iter(process.stdout.readline, ''):
-        filepath = line.decode('latin-1').rstrip()
-#        print filepath
+    diff_tree_lines = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('latin-1').split('\n')
+    for line in iter(diff_tree_lines):
+        filepath = line.rstrip()
         if filepath != '' and filepath.endswith(ext):
             files[filepath] = retrieve_diff_on_filepath(repository, commit, filepath)
     return files
 
+
 def retrieve_diff_on_filepath(repository, commit, filepath):
-    cmd = 'git -C ' + repository + ' diff --unified=0 --no-prefix ' + commit + '^ '+commit +' -- '+filepath
-#    print cmd
-    diff_lines = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE).stdout.read().decode('latin-1')
-#    print diff_lines
+    cmd = 'git -C ' + repository + ' diff --unified=0 --no-prefix ' + commit + '^ ' + commit + ' -- ' + filepath
+    diff_lines = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('latin-1')
     return diff_lines
- 
+
+
 if __name__ == "__main__":
     main()
-
-
